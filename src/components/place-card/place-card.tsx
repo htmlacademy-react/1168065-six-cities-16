eea7320d8af9170b4df2ddc1d8377f596f480d6e
A Zhila <aleksej.zhila@gmail.com>
2024-07-13 21:04:34 +0300

    Разбил Header на компоненты

--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -1,23 +1,9 @@
 import clsx from 'clsx';
 
 type PlaceCard = {
-  // id: string;
   title: string;
   type: string;
   price: number;
-  // city: {
-  //   name: string;
-  //   location: {
-  //     latitude: number;
-  //     longitude: number;
-  //     zoom: number;
-  //   };
-  // };
-  // location: {
-  //   latitude: number;
-  //   longitude: number;
-  //   zoom: number;
-  // };
   isFavorite: boolean;
   isPremium: boolean;
   rating: number;
