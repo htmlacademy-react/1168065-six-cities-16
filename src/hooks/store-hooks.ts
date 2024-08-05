1ef8d0c6e016e227444e4169880b6139e5042424
A Zhila <aleksej.zhila@gmail.com>
2024-08-05 22:45:48 +0300

    Добавил комментарии

--- src/hooks/store-hooks.ts
+++ src/hooks/store-hooks.ts
@@ -1,5 +1,6 @@
 import { AppDispatch, State } from '@src/entities/state';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
+// обертка над хуками редакса для типизации и подсказок
 export const useAppDispatch = () => useDispatch<AppDispatch>;
 export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
