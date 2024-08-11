6f366e044aae97b10904febdc2460b1602a0c8d6
A Zhila <aleksej.zhila@gmail.com>
2024-08-11 12:26:30 +0300

    Добавил в стор thunk и axios

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { sortingSlice } from '@src/store/slices/sorting-slice';
 import { offersSlice } from '@src/store/slices/offers-slice';
+import { createAPI } from '@src/services/api';
+
+export const api = createAPI();
 
 const store = configureStore({
   reducer: {
     [offersSlice.name]: offersSlice.reducer,
     [sortingSlice.name]: sortingSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
