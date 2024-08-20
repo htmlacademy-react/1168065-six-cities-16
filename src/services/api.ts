cde99a7be776b060c33756796d44f0e821de15a5
Alexey Zhila <raikkonen07@live.ru>
2024-08-20 22:57:19 +0300

    Добавил вывоод уведомлений об общих сетевых ошибках

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
-import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import axios, {
+  AxiosInstance,
+  InternalAxiosRequestConfig,
+  isAxiosError,
+} from 'axios';
 import { getToken } from './token';
+import { toast } from 'react-toastify';
 
 const API_URL = 'https://16.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -10,6 +15,7 @@ export const createAPI = (): AxiosInstance => {
     timeout: REQUEST_TIMEOUT,
   });
 
+  // добавляем токен к запросу
   api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
     const token = getToken();
 
@@ -18,6 +24,19 @@ export const createAPI = (): AxiosInstance => {
     }
 
     return config;
+  });
+
+  // вывод сообщений об общих ошибках
+  api.interceptors.response.use(null, (error) => {
+    if (isAxiosError(error)) {
+      if (error.code === 'ERR_NETWORK') {
+        toast.error('Network connection error');
+      }
+
+      if (error.response && error.response.status >= 500) {
+        toast.error('Server response error');
+      }
+    }
   });
 
   return api;
