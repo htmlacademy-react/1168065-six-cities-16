2da2d0ca95498cf73b88bb085528394d03d73c48
Alexey Zhila <raikkonen07@live.ru>
2024-08-27 18:57:15 +0300

    Добавил тест для функции capitalizeFirstLetter

--- src/utils/formatters.test.ts
+++ src/utils/formatters.test.ts
@@ -1,16 +1,24 @@
-import { pluralize } from './formatters';
+import { capitalizeFirstLetter, pluralize } from './formatters';
 
-describe('Function: pluralize', () => {
-  it('should return singular form when count is 1', () => {
-    expect(pluralize(1, 'offer')).toBe('1 offer');
+describe('String formatting functions', () => {
+  describe('Function: capitalizeFirstLetter', () => {
+    it('should return capitalized first letter for provided string', () => {
+      expect(capitalizeFirstLetter('appartment')).toBe('Appartment');
+    });
   });
 
-  it('should return plural form when count is not 1', () => {
-    expect(pluralize(0, 'offer')).toBe('0 offers');
-    expect(pluralize(2, 'offer')).toBe('2 offers');
-  });
+  describe('Function: pluralize', () => {
+    it('should return singular form when count is 1', () => {
+      expect(pluralize(1, 'offer')).toBe('1 offer');
+    });
 
-  it('should use custom suffix if provided', () => {
-    expect(pluralize(2, 'cactus', 'i')).toBe('2 cactusi');
+    it('should return plural form when count is not 1', () => {
+      expect(pluralize(0, 'offer')).toBe('0 offers');
+      expect(pluralize(2, 'offer')).toBe('2 offers');
+    });
+
+    it('should use custom suffix if provided', () => {
+      expect(pluralize(2, 'cactus', 'i')).toBe('2 cactusi');
+    });
   });
 });
